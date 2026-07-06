579f46741238833f39111be26eefb69b1c14a92a
RafaelMourao <mourao.ea@gmail.com>
2026-07-06 12:35:46 -0300

    feat: add route for RecipesPage in the main application

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage.tsx";
 import { MarketDataPage } from "./pages/MarketDataPage.tsx";
+import { RecipesPage } from "./pages/RecipesPage.tsx";
 
 export function App() {
   return (
@@ -8,6 +9,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/idleclans/market" element={<MarketDataPage />} />
+        <Route path="/idleclans/recipes" element={<RecipesPage />} />
       </Routes>
     </BrowserRouter>
   );
